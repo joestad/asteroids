808b5526f8a99b38244b0ebb9b48e3e8203734e2
RexRakete <johannes-stadler@web.de>
2024-10-18 22:53:07 +0200

    added black screen, added .gitignore

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+#imports pygame
 import pygame
+
+#imports constants
 from constants import *
 
+
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     main()
