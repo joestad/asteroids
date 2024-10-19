676eda72bbedb6c6d73f4743a10885403e47fb28
RexRakete <johannes-stadler@web.de>
2024-10-19 12:16:15 +0200

    limit refresh rate to 60 FPS

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 
