5e270a4477ccba226af825cd69e6b5345a14794b
RexRakete <johannes-stadler@web.de>
2024-10-20 13:40:43 +0200

    added player Triangle and rotation movement

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pygame
 #imports constants
 from constants import *
 
+#imports player
+from player import Player
+
 
 def main():
     
@@ -12,12 +15,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0,0,0))
+
+        player.draw(screen)
+
+        player.update(dt)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
